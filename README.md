6b9bfc316007847f8d8e5eab979d0b837813131b
Ricardo Branco <rbranco@suse.de>
2019-06-18 14:21:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# restartable
+List possibly restartable processes or services
+
+[![Build Status](https://travis-ci.org/ricardobranco777/restartable.svg?branch=master)](https://travis-ci.org/ricardobranco777/restartable)
+
+When upgrading packages, the package manager won't restart services or programs that depend on some updated library, for obvious reasons.  This has security implications in the case of outdated libraries still in use.
+
+OpenSUSE has a cool `zypper ps` command to check for programs and services using deleted files.
+It's also available in other distros as `zypp-CheckAccessDeleted` from the `libzypp` package.
+Those commands parse `lsof` output and check `/proc/<pid>/cgroup` to extract the service names.
+
+Here's a Python version that checks `/proc/<pid>/exe` & `/proc/<pid>/map_files`,
+extracting information also from `/proc/<pid>/cmdline`, `/proc/<pid>/stat` & `/proc/<pid>/status`
+
+No `lsof` dependency needed.
+
+Excerpt from `zypper` manual:
+
+> After each upgrade or removal of packages, there may be running processes on the system which continue to use meanwhile deleted files. zypper ps lists all processes using deleted files, together
+> with the corresponding files, and a service name hint, in case it’s a known service. This gives a hint which services may need to be restarted after an update. Usually programs which continue to
+> use deleted shared libraries. The list contains the following information:
+>
+
+|   |   |
+|---|---|
+| PID | ID of the process
+| PPID | ID of the parent process
+| UID | ID of the user running the process
+| Login | Login name of the user running the process
+| Command | Command used to execute the process
+| Service | Service name, if command is associated with a system service
+| Files | The list of the deleted files
+
+## Usage
+
+```
+restartable.py [OPTIONS]
+
+List running processes using files deleted by recent upgrades
+
+Options:
+    -s, --short
+         Create a short table not showing the deleted files. Given twice,
+         show only processes which are associated with a system service.
+         Given three times, list the associated system service names only.
+```
+
+## NOTES
+
+  - Verified to work on Python 2.7 & 3.5+
+  - You must run it as root to see the information you want.
+  - The output is different from `zypper ps` to be easily parseable
+
+## BUGS / LIMITATIONS
+
+  - The service names can only be extracted if started by Systemd
---
+++ restartable.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 Ricardo Branco <rbranco@suse.de>
+# MIT License
+#
+"""
+list restartable programs or services using deleted libraries
+"""
+
+from __future__ import print_function
+
+import os
+import pwd
+import re
+import sys
+from argparse import ArgumentParser
+from collections import namedtuple
+from glob import glob
+
+VERSION = "0.3"
+
+USAGE = """%s [OPTIONS]
+
+List running processes using files deleted by recent upgrades
+
+Options:
+    -h, --help      Get help
+    -V, --version   Show version and exit
+    -s, --short
+         Create a short table not showing the deleted files. Given twice,
+         show only processes which are associated with a system service.
+         Given three times, list the associated system service names only.
+""" % os.path.basename(sys.argv[0])
+
+# Ignore deleted files in these directories
+IGNORE = ('/dev',
+          '/home',
+          '/i915',
+          '/memfd:',
+          '/run',
+          '/SYSV',
+          '/tmp',
+          '/var',
+          '/[aio]')
+
+# Regular expression to find systemd service unit in /proc/<pid>/cgroup
+SYSTEMD_REGEX = r"[0-9]+:name=systemd:/system.slice/(?:.*/)?(.*).service$"
+
+
+def get_stat_fields():
+    """
+    Return the names of stat fields supported by this kernel
+    """
+    # /proc/<pid>/stat fields as described by procfs(5)
+    fields = ('pid comm state ppid pgrp session tty_nr tpgid flags '
+              'minflt cminflt majflt cmajflt utime stime cutime cstime '
+              'priority nice num_threads itrealvalue starttime vsize rss '
+              'rsslim startcode endcode startstack kstkesp kstkeip signal '
+              'blocked sigignore sigcatch wchan nswap cnswap exit_signal '
+              'processor rt_priority policy delayacct_blkio_ticks guest_time '
+              'cguest_time start_data end_data start_brk arg_start arg_end '
+              'env_start env_end exit_code')
+    with open("/proc/1/stat") as file:
+        data = re.findall(r"\(.*\)|\S+", file.read().strip())
+    return fields.split()[:len(data)]
+
+
+# Create named tuple with stat fields as attributes
+Stat = namedtuple('Stat', get_stat_fields())
+
+
+# Create named tuple with /proc/<pid>/status fields as attributes
+with open("/proc/1/status") as _file:
+    Status = namedtuple(
+        'Status',
+        [_.split(':')[0] for _ in _file.read().splitlines()])
+
+
+# Create named tuple for UID & GID
+ID = namedtuple('ID', ('real', 'effective', 'saved_set', 'filesystem'))
+
+args = None
+
+
+def parse_proc_status(path):
+    """
+    Parse /proc/<PID>/status
+    Returns Status named tuple
+    """
+    with open(path) as file:
+        lines = file.read().splitlines()
+    return Status(*[_.split(':')[1].strip() for _ in lines])
+
+
+def parse_proc_stat(path):
+    """
+    Parse /proc/<PID>/stat
+    Returns Stat named tuple
+    """
+    with open(path) as file:
+        data = re.findall(r"\(.*\)|\S+", file.read().strip())
+    return Stat(*data)
+
+
+def parse_status_id(xid):
+    """
+    Parse the Uid/Gid field of /proc/<PID>/status
+    Returns ID named tuple
+    """
+    return ID(*map(int, xid.split()))
+
+
+def print_info(pid, deleted):
+    """
+    Print information
+    """
+    try:
+        with open("cmdline") as file:
+            cmdline = file.read().replace("\0", " ")
+        with open("cgroup") as file:
+            cgroup = file.read()
+        status = parse_proc_status("status")
+        stat = parse_proc_stat("stat")
+    except OSError:
+        return
+    # cmdline is empty if zombie
+    if not cmdline:
+        cmdline = stat.comm
+    uid = parse_status_id(status.Uid)
+    try:
+        username = pwd.getpwuid(uid.real).pw_name
+    except KeyError:
+        username = "-"
+    try:
+        service = re.findall(SYSTEMD_REGEX, cgroup, re.M)[0]
+    except IndexError:
+        if args.short > 1:
+            return
+        service = "-"
+    if args.short > 2:
+        print(service)
+    else:
+        print("%s\t%s\t%s\t%s\t%40s\t%s" % (
+            pid, stat.ppid, uid.real, username, service, cmdline))
+    if not args.short:
+        for path in sorted(deleted):
+            print("\t%s" % path[:-len(" (deleted)")])
+
+
+def main():
+    """
+    Main function
+    """
+    argparser = ArgumentParser(usage=USAGE, add_help=False)
+    argparser.add_argument('-h', '--help', action='store_true')
+    argparser.add_argument('-s', '--short', action='count', default=0)
+    argparser.add_argument('-V', '--version', action='store_true')
+    global args
+    args = argparser.parse_args()
+    if args.help:
+        print(USAGE)
+        sys.exit(0)
+    elif args.version:
+        print(VERSION)
+        sys.exit(0)
+
+    if os.geteuid() != 0:
+        print("WARN: Run this program as root", file=sys.stderr)
+
+    if args.short < 3:
+        print("%s\t%s\t%s\t%s\t%40s\t%s" % (
+            "PID", "PPID", "UID", "User", "Service", "Command"))
+    for pid in map(os.path.basename, glob("/proc/[0-9]*")):
+        try:
+            os.chdir("/proc/%s" % pid)
+            map_files = ["map_files/" + f for f in os.listdir("map_files/")]
+        except OSError:
+            continue
+        deleted = set()
+        for path in ["exe"] + map_files:
+            try:
+                link = os.readlink(path)
+            except OSError:
+                pass
+            if not link or link == "/ (deleted)":
+                continue
+            if link.endswith(' (deleted)') and not link.startswith(IGNORE):
+                deleted.add(link)
+        if deleted:
+            print_info(pid, deleted)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
