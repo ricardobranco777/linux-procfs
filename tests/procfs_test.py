4c24a1821e99a0f4e98dd45774449322df7276d0
Ricardo Branco <rbranco@suse.de>
2019-10-28 19:37:28 +0100

    procfs: Add unit tests for procfs.ProcPid

--- tests/procfs_test.py
+++ tests/procfs_test.py
@@ -1,7 +1,14 @@
+import os
 import unittest
+from collections import namedtuple
+from resource import getrlimit, RLIMIT_STACK
+from mock import patch, mock_open
 
 from restartable.procfs import Proc, ProcNet, ProcPid
-from restartable.utils import AttrDict, IPAddr, Uid
+from restartable.utils import AttrDict, IPAddr, Uid, Gid, Time
+
+
+# pylint: disable=no-member,unsubscriptable-object,unsupported-delete-operation
 
 
 class Test_ProcNet(unittest.TestCase):
@@ -13,7 +20,7 @@ class Test_ProcNet(unittest.TestCase):
             self.assertIsInstance(net_, ProcNet)
             self.assertEqual(p.net.tcp[0], p_.net.tcp[0])
 
-    def test_ProcNet_protos(self):
+    def test_protos(self):
         with Proc() as p:
             net = p.net
             assert net.tcp
@@ -29,7 +36,7 @@ class Test_ProcNet(unittest.TestCase):
             self.assertIsInstance(net.tcp[0].local_port, int)
             self.assertIsInstance(net.tcp[0].uid, Uid)
 
-    def test_ProcNet_arp(self):
+    def test_arp(self):
         with Proc() as p:
             net = p.net
             assert net.arp
@@ -43,7 +50,7 @@ class Test_ProcNet(unittest.TestCase):
             self.assertEqual(net.data, {})
             self.assertIsInstance(net.arp[0].IP_address, IPAddr)
 
-    def test_ProcNet_dev(self):
+    def test_dev(self):
         with Proc() as p:
             net = p.net
             assert net.dev
@@ -57,7 +64,7 @@ class Test_ProcNet(unittest.TestCase):
             self.assertEqual(net.data, {})
             self.assertIsInstance(net.dev['lo'].RX_bytes, int)
 
-    def test_ProcNet_netstat(self):
+    def test_netstat(self):
         with Proc() as p:
             net = p.net
             assert net.netstat
@@ -73,7 +80,7 @@ class Test_ProcNet(unittest.TestCase):
             self.assertEqual(net.data, {})
             self.assertIsInstance(net.netstat['TcpExt'].SyncookiesSent, int)
 
-    def test_ProcNet_snmp(self):
+    def test_snmp(self):
         with Proc() as p:
             net = p.net
             assert net.snmp
@@ -87,7 +94,7 @@ class Test_ProcNet(unittest.TestCase):
             self.assertEqual(net.data, {})
             self.assertIsInstance(net.snmp['Ip'].Forwarding, int)
 
-    def test_ProcNet_route(self):
+    def test_route(self):
         with Proc() as p:
             net = p.net
             assert net.route
@@ -102,7 +109,7 @@ class Test_ProcNet(unittest.TestCase):
             for key in ('Destination', 'Gateway', 'Mask'):
                 self.assertIsInstance(net.route[0][key], IPAddr)
 
-    def test_ProcNet_unix(self):
+    def test_unix(self):
         with Proc() as p:
             net = p.net
             assert net.unix
@@ -113,3 +120,156 @@ class Test_ProcNet(unittest.TestCase):
             del net['unix']
             self.assertEqual(net.data, {})
             self.assertIsInstance(net.unix[0], AttrDict)
+
+
+class Test_ProcPid(unittest.TestCase):
+    def test_ProcPid(self):
+        with ProcPid() as p, ProcPid(os.getpid()) as p_:
+            self.assertIsInstance(p, ProcPid)
+            self.assertEqual(p.pid, p_.pid)
+            self.assertEqual(p.ctime, p_.ctime)
+
+    @patch('os.stat', return_value=namedtuple('_', 'st_ctime')(float(0)))
+    def test_ctime(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.ctime, Time)
+            self.assertEqual(p.ctime.datetime.ctime(), 'Thu Jan  1 00:00:00 1970')
+
+    @patch('builtins.open', mock_open(read_data="a\nb\0c\0"))
+    def test_cmdline(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.cmdline, list)
+            self.assertEqual(p.cmdline, ["a\\nb", "c"])
+            self.assertEqual(p.cmdline, p['cmdline'])
+            del p.cmdline
+            self.assertEqual(p.cmdline, p['cmdline'])
+            del p['cmdline']
+            self.assertEqual(p.data, {})
+
+    @patch('builtins.open', mock_open(read_data="ab\n"))
+    def test_comm(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.comm, str)
+            self.assertEqual(p.comm, "ab")
+            self.assertEqual(p.comm, p['comm'])
+            del p.comm
+            self.assertEqual(p.comm, p['comm'])
+            del p['comm']
+            self.assertEqual(p.data, {})
+
+    def test_environ1(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.environ, AttrDict)
+            self.assertEqual(p.environ, os.environ)
+            self.assertEqual(p.environ, p['environ'])
+            del p.environ
+            self.assertEqual(p.environ, p['environ'])
+            del p['environ']
+            self.assertEqual(p.data, {})
+
+    @patch('builtins.open', mock_open(read_data=b'\xff\x00'))
+    def test_environ2(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.environ, bytes)
+            self.assertEqual(p.environ, b'\xff\x00')
+
+    @patch('builtins.open', mock_open(read_data="a: 1\nb: 2\n"))
+    def test_io(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.io, AttrDict)
+            self.assertEqual(p.io, {'a': 1, 'b': 2})
+            self.assertEqual(p.io, p['io'])
+            del p.io
+            self.assertEqual(p.io, p['io'])
+            del p['io']
+            self.assertEqual(p.data, {})
+
+    def test_limits(self):
+        with ProcPid() as p:
+            self.assertIsInstance(p.limits, AttrDict)
+            self.assertEqual(getrlimit(RLIMIT_STACK), (p.limits.stack.soft, p.limits.stack.hard))
+            self.assertEqual(p.limits, p['limits'])
+            del p.limits
+            self.assertEqual(p.limits, p['limits'])
+            del p['limits']
+            self.assertEqual(p.data, {})
+
+    def test_maps(self):
+        with ProcPid() as p:
+            self.assertIsInstance(p.maps[0], AttrDict)
+            address = "-".join(map(lambda _: _.lstrip('0'), p.maps[0].address.split('-')))
+            assert address in p.map_files
+            self.assertEqual(p.maps, p['maps'])
+            del p.maps
+            self.assertEqual(p.maps, p['maps'])
+            del p['maps']
+            self.assertEqual(p.data, {})
+
+    @patch('builtins.open', mock_open(read_data="tmpfs /dev/shm tmpfs rw,nosuid,nodev 0 0\n"))
+    def test_mounts(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.mounts[0], AttrDict)
+            self.assertEqual(p.mounts[0].fs_spec, "tmpfs")
+            self.assertEqual(p.mounts[0].fs_file, "/dev/shm")
+            self.assertEqual(p.mounts[0].fs_vfstype, "tmpfs")
+            self.assertEqual(p.mounts[0].fs_mntops, "rw,nosuid,nodev")
+            self.assertEqual(p.mounts[0].fs_freq, "0")
+            self.assertEqual(p.mounts[0].fs_passno, "0")
+            self.assertEqual(p.mounts, p['mounts'])
+            del p.mounts
+            self.assertEqual(p.mounts, p['mounts'])
+            del p['mounts']
+            self.assertEqual(p.data, {})
+
+    @patch('builtins.open', mock_open(read_data="777 ( a \n b ) S" + " 1" * 49))
+    def test_stat(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.stat, AttrDict)
+            self.assertEqual(p.stat.comm, " a \\n b ")
+            self.assertEqual(p.stat, p['stat'])
+            del p.stat
+            self.assertEqual(p.stat, p['stat'])
+            del p['stat']
+            self.assertEqual(p.data, {})
+
+    @patch('builtins.open', mock_open(read_data="0 1 2 3 4 5 6"))
+    def test_statm(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.statm, AttrDict)
+            for value, key in enumerate('size resident shared text lib data dt'.split()):
+                self.assertEqual(p.statm[key], value)
+            self.assertEqual(p.statm, p['statm'])
+            del p.statm
+            self.assertEqual(p.statm, p['statm'])
+            del p['statm']
+            self.assertEqual(p.data, {})
+
+    @patch('builtins.open', mock_open(read_data="Uid:\t0 1 2 3\nGid:\t4 5 6 7\nGroups:\t0 1\n"))
+    def test_status(self, *_):
+        with ProcPid() as p:
+            self.assertIsInstance(p.status, AttrDict)
+            self.assertIsInstance(p.status.Uid.real, Uid)
+            self.assertIsInstance(p.status.Gid.real, Gid)
+            self.assertIsInstance(p.status.Groups[0], Gid)
+            for value, key in enumerate('real effective saved_set filesystem'.split()):
+                self.assertEqual(p.status.Uid[key], str(value))
+                self.assertEqual(p.status.Gid[key], str(value + 4))
+            for i, group in enumerate(p.status.Groups):
+                self.assertEqual(group, str(i))
+            self.assertEqual(p.status, p['status'])
+            del p.status
+            self.assertEqual(p.status, p['status'])
+            del p['status']
+            self.assertEqual(p.data, {})
+
+    def test_fd(self):
+        with ProcPid() as p:
+            self.assertIsInstance(p.task, list)
+            self.assertEqual(p.fd, p['fd'])
+            self.assertEqual(p['fd/0'], os.ttyname(0))
+
+    def test_task(self):
+        with ProcPid() as p:
+            self.assertIsInstance(p.task, list)
+            self.assertEqual(p.task, p['task'])
+            assert p.pid in p.task
